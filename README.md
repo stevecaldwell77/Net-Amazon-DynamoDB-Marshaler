d599632cd9206a7c08ca207bdc95959c69914ae3
Stephen Caldwell <steve@campusexplorer.com>
2017-07-10 11:44:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Net::Amazon::DynamoDB::Marshaler - Blah blah blah
+
+# SYNOPSIS
+
+    use Net::Amazon::DynamoDB::Marshaler;
+
+# DESCRIPTION
+
+Net::Amazon::DynamoDB::Marshaler is
+
+# AUTHOR
+
+Steve Caldwell <scaldwell@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2017- Steve Caldwell
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Net/Amazon/DynamoDB/Marshaler.pm
@@ -0,0 +1,39 @@
+package Net::Amazon::DynamoDB::Marshaler;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::Amazon::DynamoDB::Marshaler - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Net::Amazon::DynamoDB::Marshaler;
+
+=head1 DESCRIPTION
+
+Net::Amazon::DynamoDB::Marshaler is
+
+=head1 AUTHOR
+
+Steve Caldwell E<lt>scaldwell@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2017- Steve Caldwell
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
