44c8228dcd2a28824d5ab10958c7e23ee8335b96
Stephen Caldwell <steve@campusexplorer.com>
2017-07-21 15:50:59 +0000

    0.05

--- lib/Net/Amazon/DynamoDB/Marshaler.pm
+++ lib/Net/Amazon/DynamoDB/Marshaler.pm
@@ -2,7 +2,7 @@ package Net::Amazon::DynamoDB::Marshaler;
 
 use strict;
 use 5.008_005;
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 use parent qw(Exporter);
 our @EXPORT = qw(dynamodb_marshal dynamodb_unmarshal);
